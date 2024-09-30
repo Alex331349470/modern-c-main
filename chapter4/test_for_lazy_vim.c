8e35f7bab5779ffb4ce74d4e275844ea6e8681f2
alex chan <331349470@qq.com>
2024-09-30 13:47:39 +0800

    remove unuseful lines

--- chapter4/test_for_lazy_vim.c
+++ chapter4/test_for_lazy_vim.c
@@ -7,12 +7,6 @@ int main(void) {
   scanf("%d", &num);
 
   printf("The entered number is %d\n", num);
-  printf("this is how to write with the neovim editor, and it's awesome for "
-         "and this good thing is switch for the mathmatic and the text editor "
-         ", when I was young and I don't notice the goodthing is complex and "
-         "simple, so now I am happy what thing is good that I realize to know"
-         "that, thanks to the worst that I notice the goodthing is very "
-         "precious\n");
 
   return 0;
 }
