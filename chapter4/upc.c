261a674444306a5c318308c817960355a5c72238
alex chan <331349470@qq.com>
2024-10-08 15:29:53 +0800

    finish the chapter4 already

--- chapter4/upc.c
+++ chapter4/upc.c
@@ -4,12 +4,12 @@ int main(void) {
   int d, i1, i2, i3, i4, i5, j1, j2, j3, j4, j5, total;
 
   printf("Enter the first 11 digits of a UPC: ");
-  scanf("%d%d%d%d%d%d%d%d%d%d%d", &d, &i1, &i2, &i3, &i4, &i5, &j1, &j2, &j3,
-        &j4, &j5);
+  scanf("%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d", &d, &i1, &i2, &i3, &i4, &i5, &j1,
+        &j2, &j3, &j4, &j5);
 
   total = 3 * (i1 + i3 + i5 + j1 + j3 + j5) + (i2 + i4 + j2 + j4) - 1;
 
-  printf("Check digit: %d\n", 9 - (total % 10));
+  printf("Check digit: %d\n", 9 - ((total - 1) % 10));
 
   return 0;
 }
