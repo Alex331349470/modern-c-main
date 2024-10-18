3992cb89c9ff77fb5a0d7a7bc7bc4aafdb15fc3f
alex chan <331349470@qq.com>
2024-10-18 15:58:21 +0800

    finish the chapter7 content

--- chapter7/length2.c
+++ chapter7/length2.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+typedef int Bool;
+typedef double Dollars;
+typedef int Quantity;
+typedef long int ptrdiff_t;
+typedef unsigned int size_t;
+typedef int wchar_t;
 
 int main(void)
 {
     int len = 0;
+    bool checkout = false;
+
+    Bool flag;
+
+    Dollars cash_in, cash_out;
+
+    Quantity stock;
 
     printf("Enter a message: ");
     while(getchar() != '\n')
